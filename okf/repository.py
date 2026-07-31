679f343e6df38a67753f08e2595b05fb1e613d8b
Mahima <mrrn.pune@gmail.com>
2026-07-31 17:02:48 +0530

    Adds Loader function

--- okf/repository.py
+++ okf/repository.py
@@ -1,7 +1,15 @@
+from .parser import parse_document
+from .utils  import find_markdown_files
+
 class OKFRepository:
     def __init__(self, root):
         self.root = root
         self.documents = {}
 
     def load(self):
-        pass
+        files = find_markdown_files(self.root)
+        
+        for file in files:
+            document = parse_document(file)
+
+            self.documents[document.title] = document
